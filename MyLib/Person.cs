91f7418fe9cb7df85899f30682fbc9ed16e4fd50
Krzysztof Zdulski <krzys.zdulski@gmail.com>
2021-03-03 10:58:29 +0100

    Fix string formatting in Person.cs

---
+++ MyLib/Person.cs
@@ -0,0 +1,47 @@
+using System.Diagnostics;
+
+namespace MyLib
+{
+    public abstract class Person
+    {
+        #region Constructors
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        #endregion
+
+        #region Properties and Fields
+
+        public Sex Sex { get; set; }
+
+        public string Name
+        {
+            get => _name;
+
+            internal set
+            {
+                Debug.Assert(!string.IsNullOrWhiteSpace(value));
+
+                _name = value;
+            }
+        }
+
+        private string _name;
+
+        #endregion
+
+        #region Methods
+
+        public virtual string GetDescription()
+        {
+            return $"{Name} of sex {Sex}";
+        }
+
+        public abstract bool CanOperate();
+
+        #endregion
+    }
+}
