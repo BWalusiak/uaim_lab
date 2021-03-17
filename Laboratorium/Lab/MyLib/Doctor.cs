698d5be9315f1be918cbdc45cd2fc754f608adbd
Krzysztof Zdulski <krzys.zdulski@gmail.com>
2021-03-17 11:22:57 +0100

    Add doctor specialization

--- Laboratorium/Lab/MyLib/Doctor.cs
+++ Laboratorium/Lab/MyLib/Doctor.cs
@@ -1,13 +1,18 @@
+using System.Collections.Generic;
+
 namespace MyLib
 {
     public class Doctor : Person
     {
-        public Doctor(string name, bool isSurgeon) : base(name)
+        public Doctor(string name, bool isSurgeon, List<int> specializations) : base(name)
         {
             IsSurgeon = isSurgeon;
+            Specializations = specializations;
         }
 
         public bool IsSurgeon { get; set; }
+
+        public List<int> Specializations { get; }
 
         public override bool CanOperate()
         {
--- Laboratorium/Lab/MyLib/PersonRepository.cs
+++ Laboratorium/Lab/MyLib/PersonRepository.cs
@@ -9,8 +9,8 @@ namespace MyLib
         {
             new Patient("Jan kowalski") {Sex = Sex.Male},
             new Patient("Anna Smith") {Sex = Sex.Female},
-            new Doctor("Stefan Nowak", true) {Sex = Sex.Male},
-            new Doctor("Marianna Kowalska", false) {Sex = Sex.Female}
+            new Doctor("Stefan Nowak", true,new List<int> {10, 34, 87}) {Sex = Sex.Male},
+            new Doctor("Marianna Kowalska", false, new List<int>{23, 98, 56}) {Sex = Sex.Female}
         };
 
         public Person[] Find(Sex sex)
