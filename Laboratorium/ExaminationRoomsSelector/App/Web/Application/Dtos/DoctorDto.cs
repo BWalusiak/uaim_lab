4cb1d2ec57930c0fd7379cb007553ffa87b7b437
bartosz walusiak <bartosz.walusiak.b@gmail.com>
2021-04-07 15:35:04 +0200

    Add .equals to Dtos

--- Laboratorium/ExaminationRoomsSelector/App/Web/Application/Dtos/DoctorDto.cs
+++ Laboratorium/ExaminationRoomsSelector/App/Web/Application/Dtos/DoctorDto.cs
@@ -7,5 +7,23 @@ namespace ExaminationRoomsSelector.Web.Application.Dtos
         public int Id { get; set; }
         public IEnumerable<int> Specializations { get; set; }
         public string Name { get; set; }
+
+        protected bool Equals(DoctorDto other)
+        {
+            return Id == other.Id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((DoctorDto) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
     }
 }
--- Laboratorium/ExaminationRoomsSelector/App/Web/Application/Dtos/ExaminationRoomDto.cs
+++ Laboratorium/ExaminationRoomsSelector/App/Web/Application/Dtos/ExaminationRoomDto.cs
@@ -6,5 +6,23 @@ namespace ExaminationRoomsSelector.Web.Application.Dtos
     {
         public string Number { get; set; }
         public IEnumerable<int> Certifications { get; set; }
+
+        protected bool Equals(ExaminationRoomDto other)
+        {
+            return Number == other.Number;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((ExaminationRoomDto) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Number != null ? Number.GetHashCode() : 0);
+        }
     }
 }
