4be78645091d53f751c3bc904eb63309af723ae4
bartosz walusiak <bartosz.walusiak.b@gmail.com>
2021-05-25 19:21:20 +0200

    Add lab web app

---
+++ Laboratorium/WebApp/Model/Service/NetworkClientFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace Model.Service
+{
+    public static class NetworkClientFactory
+    {
+        public static INetwork GetNetworkClient()
+        {
+#if DEBUG
+            return new FakeNetworkClient();
+#else
+            const string serviceHost = "roomselector-app";
+            const int servicePort = 80;
+
+            return new NetworkClient( serviceHost, servicePort );
+#endif
+        }
+    }
+}
